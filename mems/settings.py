b518f8b1fcb598aacb90e65cae79c5cf34b7cc6b
Raga <ragawilliam570@gmail.com>
2023-06-18 22:38:57 +0300

    Configure apps, urls and database

--- mems/settings.py
+++ mems/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'enterprise',
 ]
 
 MIDDLEWARE = [
@@ -69,14 +70,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mems.wsgi.application'
 
+AUTH_USER_MODEL = 'enterprise.CustomUser'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mems',
+        'USER': 'mems_usr',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c search_path=public',
+        },
     }
 }
 
