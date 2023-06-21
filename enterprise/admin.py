01e2114b77281225882cbc5a475dc932c74b19c6
Raga <ragawilliam570@gmail.com>
2023-06-21 21:03:26 +0300

    Register models to django admin panel

--- enterprise/admin.py
+++ enterprise/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Product, Order, Loan, OrderItem, Address, Account, Transaction
+
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Loan)
+admin.site.register(OrderItem)
+admin.site.register(Address)
+admin.site.register(Account)
+admin.site.register(Transaction)
+# admin.site.register()
+# admin.site.register()
