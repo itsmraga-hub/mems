4fdb9e4fae9eb45c8a6de4c8596ad050a29e35eb
Raga <ragawilliam570@gmail.com>
2023-06-22 23:01:46 +0300

    Add functionality to add a product

---
+++ enterprise/add_product.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+
+import json
+import uuid
+
+from .models import Product
+
+class SaveProduct:
+  def __init__(self):
+    pass
+
+  def save_new_product(self, name, description, price, image, quantity, brand, category):
+    try:
+      p_code = str(uuid.uuid4())
+      product = Product(
+        p_code=p_code,
+        name=name,
+        description=description,
+        price=price,
+        image=image, 
+        quantity=quantity,
+        brand=brand,
+        category=category
+      )
+      print(product)
+      product.save()
+      return HttpResponse(json.dumps({"status": "success", "data": {"message": p_code}}))
+    except Exception as e:
+      return HttpResponse(json.dumps({"status": "fail", "data": {"message": str(e)}}))
