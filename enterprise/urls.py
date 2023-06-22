20720e1912a23ee26e682add1fd2dd5a1e179f7d
Raga <ragawilliam570@gmail.com>
2023-06-22 23:02:38 +0300

    Add and rename urls

--- enterprise/urls.py
+++ enterprise/urls.py
@@ -6,8 +6,14 @@ from . import views
 
 urlpatterns = [
     path('', views.homeview, name='home'),
-    path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('clients/signup', views.client_signup, name='client_signup'),
+    path('clients/login', views.client_login, name='client_login'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('products/new', views.add_product, name='add_product'),
+    path('products', views.products, name='products')
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
