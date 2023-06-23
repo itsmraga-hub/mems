5914a0d5b93b8192dd144c7c7651277d6b8b9b15
Raga <ragawilliam570@gmail.com>
2023-06-23 18:42:49 +0300

    Add url route to logout

--- enterprise/urls.py
+++ enterprise/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
@@ -9,6 +10,7 @@ urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('clients/signup', views.client_signup, name='client_signup'),
     path('clients/login', views.client_login, name='client_login'),
+    path('/signout', LogoutView.as_view(), name='signout'),
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('products/new', views.add_product, name='add_product'),
     path('products', views.products, name='products')
