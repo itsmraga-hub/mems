f4971d3d6524450ff88f8d797b02b792d533d973
Raga <ragawilliam570@gmail.com>
2023-06-27 17:56:36 +0300

    Register all models and render them well

--- enterprise/admin.py
+++ enterprise/admin.py
@@ -3,12 +3,30 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, Product, Order, Loan, OrderItem, Address, Account, Transaction
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display =['id', 'name', 'price', 'quantity']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display =['id', 'client', 'confirmed', 'status', 'paid']
+
+
+class LoanAdmin(admin.ModelAdmin):
+    list_display =['id', 'borrower', 'amount', 'interest_rate']
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display =['id', 'order', 'product', 'quantity', 'price']
+
+
+
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Loan)
-admin.site.register(OrderItem)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Loan, LoanAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(Address)
 admin.site.register(Account)
 admin.site.register(Transaction)
