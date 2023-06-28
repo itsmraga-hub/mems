80e0f4422e7460ba01f6b0f20c523c8194a7b6cb
Raga <ragawilliam570@gmail.com>
2023-06-28 19:35:13 +0300

    Register Expense and ExpenseCategory

--- enterprise/admin.py
+++ enterprise/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Product, Order, Loan, OrderItem, Address, Account, Transaction
+from .models import User, Product, Order, Loan, OrderItem, Address, Account, Transaction, ExpenseCategory, Expense
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -18,6 +18,13 @@ class LoanAdmin(admin.ModelAdmin):
 class OrderItemAdmin(admin.ModelAdmin):
     list_display =['id', 'order', 'product', 'quantity', 'price']
 
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    list_display =['id', 'name', 'ec_code']
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display =['id', 'date', 'staff', 'e_code', 'amount']
+
 
 
 
@@ -30,5 +37,7 @@ admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(Address)
 admin.site.register(Account)
 admin.site.register(Transaction)
+admin.site.register(ExpenseCategory, ExpenseCategoryAdmin)
+admin.site.register(Expense, ExpenseAdmin)
 # admin.site.register()
 # admin.site.register()
